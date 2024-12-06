9c5c498e639de0ddb7caff2dff5e7bc1f8a0c25d
alisadeghi <alisadghi228@gmail.com>
2024-12-06 22:06:30 +0330

    feat : show the personal info and summary into resume preview section

--- src/components/ResumePreview.tsx
+++ src/components/ResumePreview.tsx
@@ -1,5 +1,8 @@
+import useDimensions from "@/hooks/useDimensions";
 import { cn } from "@/lib/utils";
 import { ResumeValues } from "@/lib/validation";
+import Image from "next/image";
+import { useEffect, useRef, useState } from "react";
 
 interface ResumePreviewProps {
   resumeData: ResumeValues;
@@ -10,16 +13,112 @@ export default function ResumePreview({
   resumeData,
   className,
 }: ResumePreviewProps) {
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  const { width } = useDimensions(containerRef);
+
   return (
     <div
       className={cn(
-        "bg-white text-black h-fit w-full aspect-[210/297]",
+        "aspect-[210/297] h-fit w-full bg-white text-black",
         className,
       )}
+      ref={containerRef}
     >
-      <h1 className="p-6 text-3xl font-bold">
-        this test should change the size
-      </h1>
+      <div
+        className={cn("space-y-6 p-6", !width && "invisible")}
+        style={{
+          zoom: (1 / 794) * width,
+        }}
+      >
+        <PersonalInfoHeader resumeData={resumeData} />
+        <SummarySection resumeData={resumeData} />
+        <WorkExperiencesSection resumeData={resumeData} />
+      </div>
     </div>
+  );
+}
+
+interface ResumeSectionProps {
+  resumeData: ResumeValues;
+}
+
+function PersonalInfoHeader({ resumeData }: ResumeSectionProps) {
+  const { photo, firstName, lastName, jobTitle, city, country, phone, email } =
+    resumeData;
+
+  const [photoSrc, setPhotoSrc] = useState(photo instanceof File ? "" : photo);
+
+  useEffect(() => {
+    const objectUrl = photo instanceof File ? URL.createObjectURL(photo) : "";
+    if (objectUrl) setPhotoSrc(objectUrl);
+    if (photo === null) setPhotoSrc("");
+
+    return () => URL.revokeObjectURL(objectUrl);
+  }, [photo]);
+
+  return (
+    <div className="flex items-center gap-6">
+      {photoSrc && (
+        <Image
+          src={photoSrc}
+          width={100}
+          height={100}
+          alt="Author photo"
+          className="aspect-square object-cover"
+        />
+      )}
+      <div className="space-y-2.5">
+        <div className="space-y-1">
+          <p className="text-3xl font-bold">
+            {firstName} {lastName}
+          </p>
+          <p className="font-medium"> {jobTitle}</p>
+        </div>
+        <p className="text-xs text-gray-500">
+          {city} {city && country ? ", " : ""}
+          {country}
+          {(city || country) && (phone || email) ? " • " : ""}
+          {[phone, email].filter(Boolean).join(" • ")}
+        </p>
+      </div>
+    </div>
+  );
+}
+
+function SummarySection({ resumeData }: ResumeSectionProps) {
+  const { summary } = resumeData;
+  if (!summary) return null;
+  return (
+    <>
+      <hr className="border-2" />
+      <div className="break-inside-avoid space-y-3">
+        <p className="text-lg font-semibold">Professional profile</p>
+        <div className="whitespace-pre-line text-sm">{summary}</div>
+      </div>
+    </>
+  );
+}
+
+function WorkExperiencesSection({ resumeData }: ResumeSectionProps) {
+  const { workExpreinces } = resumeData;
+  const workExperiencesNotEmpty = workExpreinces?.filter(
+    (exp) => Object.values(exp).filter(Boolean).length > 0,
+  );
+
+  if (!workExperiencesNotEmpty?.length) return null;
+
+  return (
+    <>
+      <hr className="border-2" />
+      <div className="space-y-3">
+        <p className="text-lg font-semibold">Work experiences</p>
+        {workExperiencesNotEmpty.map((exp, index) => (
+          <div key={index} className="break-inside-avoid space-y-1">
+            <div className="flex items-center justify-between text-sm font-semibold"></div>
+          </div>
+        ))}
+      </div>
+    </>
   );
 }
